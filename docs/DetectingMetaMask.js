b6aed4e2e880d534813f2a104fe1e6f45d88e03e
Bertrand Juglas <bertrand.juglas@ozitem.com>
2018-06-02 20:19:44 +0200

    Detect MetaMask

---
+++ docs/DetectingMetaMask.js
@@ -0,0 +1,13 @@
+window.addEventListener('load', function() {
+
+  // Checking if Web3 has been injected by the browser (Mist/MetaMask)
+  if (typeof web3 !== 'undefined') {
+
+    // Use the browser's ethereum provider
+    var provider = web3.currentProvider
+
+  } else {
+    console.log('No web3? You should consider trying MetaMask!')
+  }
+
+})
