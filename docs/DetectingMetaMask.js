3da0920c21de72a8e688ff45d62adfed0e51efc9
Bertrand Juglas <bertrand.juglas@ozitem.com>
2018-06-02 21:54:51 +0200

    add output in the HTML object

--- docs/DetectingMetaMask.js
+++ docs/DetectingMetaMask.js
@@ -4,9 +4,11 @@ window.addEventListener('load', function() {
   if (typeof web3 !== 'undefined') {
 
     // Use the browser's ethereum provider
-    var provider = web3.currentProvider
+    var provider = web3.currentProvider;
+    document.getElementById("output").innerHTML = provider;
 
   } else {
+    document.getElementById("output").innerHTML = 'No web3? You should consider trying MetaMask!';
     console.log('No web3? You should consider trying MetaMask!')
   }
 
